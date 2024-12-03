d74b5ac4f17d2dabdcc75f7146cdf3fd2bfe3e90
Amelia Michelle Jonaidy <23002502@myrp.edu.sg>
2024-12-03 14:57:19 +0800

    inital commit

--- src/components/App.js
+++ src/components/App.js
@@ -7,21 +7,30 @@ function App() {
 
   const handleAddMovie = (title) => {
     // TODO: Implement adding a new movie to the list
+    setMovies((prevTitle) => [...prevTitle, title]);
   };
 
   const handleToggleWatched = (id) => {
     // TODO: Implement toggling a movie's watched status
+    setMovies((prevTitle) =>
+      prevTitle.map((title) =>
+        title.id === id ? { ...title, watched: !title.watched } : title
+      )
+    );
   };
 
   const handleDeleteMovie = (id) => {
     // TODO: Implement deleting a movie from the list
+    setMovies((prevTitle) => prevTitle.filter(title => title.id !==id));
   };
 
   return (
     <div>
       <h1>Favorite Movies</h1>
       {/* TODO: Add AddMovieForm Component */}
+      <AddMovieForm onAddMovie={handleAddMovie}/>
       {/* TODO: Add MovieList Component */}
+      <MovieList movies={movies} onToggleWatched={handleToggleWatched} onDeleteMovie={handleDeleteMovie}/>
     </div>
   );
 }
